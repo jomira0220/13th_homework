5caf2b885afc3d9edd41d92047b2e9e4230ec09c
jomira0220 <whalfk220@gmail.com>
2024-12-02 17:38:28 +0900

    클릭한 좌표를 기준으로 주소 노출 처리 관련 함수 추가

--- hybrid_app_homework/my-web/src/components/kakao-map/index.tsx
+++ hybrid_app_homework/my-web/src/components/kakao-map/index.tsx
@@ -13,25 +13,68 @@ interface IKakaoMap {
   lat: number;
   lng: number;
   className?: string;
+  setAddress: (address: string) => void;
 }
 
-export default function KaKaoMap({ lat, lng, className }: IKakaoMap) {
+interface IPosition {
+  lat: number;
+  lng: number;
+}
+
+export default function KaKaoMap({
+  lat,
+  lng,
+  className,
+  setAddress,
+}: IKakaoMap) {
   useKakaoLoader();
 
-  const [position, setPosition] = useState({ lat, lng });
+  const [position, setPosition] = useState<IPosition>();
+
+  useEffect(() => {
+    if (!lat || !lng) {
+      // 위치 정보가 없을 경우, 서울 시청을 기본 위치로 설정
+      setPosition({
+        lat: 37.56682,
+        lng: 126.97865,
+      });
+      fetchAddress(37.56682, 126.97865);
+    } else {
+      // 현재 위치 정보가 있을 경우, 해당 위치로 설정
+      setPosition({ lat, lng });
+      fetchAddress(lat, lng);
+    }
+  }, []);
+
+  // 클릭한 좌표로 주소를 반환하는 함수
+  const fetchAddress = async (lat: number, lng: number) => {
+    if (!window.kakao) return;
+    const geocoder = new window.kakao.maps.services.Geocoder();
+    const coord = new window.kakao.maps.LatLng(lat, lng);
+    geocoder.coord2Address(coord.getLng(), coord.getLat(), (result, status) => {
+      if (status === window.kakao.maps.services.Status.OK) {
+        const address = result[0]?.address?.address_name;
+        setAddress(address || "주소를 찾을 수 없습니다.");
+        console.log(address);
+      }
+    });
+  };
+
+  // 지도 클릭 이벤트 - 클릭한 위치 좌표 저장
+  const mapClick = (mouseEvent: any) => {
+    const lat = mouseEvent.latLng.getLat();
+    const lng = mouseEvent.latLng.getLng();
+    setPosition({ lat, lng });
+    fetchAddress(lat, lng);
+  };
 
   return (
     <>
       <Map
         className={className}
-        center={{ lat, lng }}
+        center={position || { lat, lng }}
         level={3}
-        onTileLoaded={(map) =>
-          setPosition({
-            lat: map.getCenter().getLat(),
-            lng: map.getCenter().getLng(),
-          })
-        }
+        onClick={(_, mouseEvent) => mapClick(mouseEvent)}
       >
         <MapTypeControl position={"TOPRIGHT"} />
         <ZoomControl position={"RIGHT"} />
