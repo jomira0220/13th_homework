9fb99dea3a6f7d3623982e32099830518691dc8e
jomira0220 <whalfk220@gmail.com>
2024-12-04 19:45:59 +0900

    파일 경로 수정 및 아폴로 셋팅 관련 파일 추가

---
+++ hybrid_app_homework/my-web/src/commons/settings/device-setting/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useEffect } from "react";
+
+export const FETCH_DEVICE_KEY: any = {
+  // fetchDeviceSystemForAppSet: resolve
+  // ...
+  // ...
+};
+
+export default function DeviceSetting({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    // 1. 안드로이드에서 수신 대기
+    document.addEventListener("message", (message: any) => {
+      if (!message.data) return;
+      // console.log("message", message);
+      // const response = JSON.parse(message.data);
+      // const query = Object.keys(response)[0];
+      // const resolve = FETCH_DEVICE_KEY[query];
+      // resolve({ data: response });
+      // delete FETCH_DEVICE_KEY[query];
+    });
+
+    // 2. IOS에서 수신 대기
+    window.addEventListener("message", (message: any) => {
+      if (!message.data) return;
+
+      // console.log("message", message);
+      // const response = JSON.parse(message.data);
+      // const query = Object.keys(response)[0];
+      // const resolve = FETCH_DEVICE_KEY[query];
+      // resolve({ data: response });
+      // delete FETCH_DEVICE_KEY[query];
+    });
+  }, []);
+
+  return <>{children}</>;
+}
