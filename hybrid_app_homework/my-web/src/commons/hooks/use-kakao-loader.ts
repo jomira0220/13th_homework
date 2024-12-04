8f1dd95d73dbe01a522c2e9cf096f9830e76379c
jomira0220 <whalfk220@gmail.com>
2024-12-04 19:55:35 +0900

    useKakaoLoader 및 useKakaoMap 훅 수정: 타입 정의 개선 및 기본 위치 설정 로직 추가

--- hybrid_app_homework/my-web/src/commons/hooks/use-kakao-loader.ts
+++ hybrid_app_homework/my-web/src/commons/hooks/use-kakao-loader.ts
@@ -1,6 +1,8 @@
+"use client";
+
 import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk";
 
-export default function useKakaoLoader() {
+export function useKakaoLoader() {
   useKakaoLoaderOrigin({
     appkey: `${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}` || "",
     libraries: ["clusterer", "drawing", "services"],
--- hybrid_app_homework/my-web/src/commons/hooks/use-kakao-map.ts
+++ hybrid_app_homework/my-web/src/commons/hooks/use-kakao-map.ts
@@ -1,32 +1,56 @@
+"use client";
+
 import { useEffect, useState } from "react";
-import { set } from "react-hook-form";
+import { useParamsControl } from "@/commons/hooks/use-params-control";
+import type { UseFormReturn } from "react-hook-form";
+import type { ISolplaceLogsSchema } from "@/commons/schema/solplace-logs/form.schema";
 
-interface IPosition {
-  lat: number;
-  lng: number;
+interface IUseKakaoMapProps {
+  methods?: UseFormReturn<ISolplaceLogsSchema>;
+  lat?: number; // kakap map에서 직접 지정한 기본값
+  lng?: number;
 }
 
-interface IUseKakaoMap {
-  position: IPosition;
-  address: string;
-}
+export function useKakaoMap({ methods, lat, lng }: IUseKakaoMapProps = {}) {
+  const { addOrUpdateQueryParams, params, searchParams } = useParamsControl();
 
-export function useKakaoMap(props: IUseKakaoMap) {
   const [openMap, setOpenMap] = useState<boolean>(false);
-  const [position, setPosition] = useState<IPosition>(props.position);
-  const [address, setAddress] = useState<string>(props.address);
+  const [position, setPosition] = useState<{
+    lat: number;
+    lng: number;
+  }>({
+    lat: lat ?? 37.5666,
+    lng: lng ?? 126.979,
+  });
+  const [address, setAddress] =
+    useState<string>("서울특별시 중구 세종대로 110");
+
+  // params.showmap 의 값에 따라서 지도를 열거나 닫음
+  useEffect(() => {
+    setOpenMap(params.showmap === "true");
+    if (params.lat && params.lng) {
+      setPosition({ lat: Number(params.lat), lng: Number(params.lng) });
+    }
+    if (params.address) {
+      setAddress(params.address);
+    }
+    methods?.setValue("lat", Number(params.lat));
+    methods?.setValue("lng", Number(params.lng));
+    methods?.setValue("address", params.address);
+    methods?.trigger();
+  }, [searchParams]);
 
   useEffect(() => {
+    if (params.showmap === "false" || !params.showmap) return;
     if (!position.lat || !position.lng) {
       // 위치 정보가 없을 경우, 서울 시청을 기본 위치로 설정
-      setPosition({
-        lat: 37.56682,
-        lng: 126.97865,
-      });
-      fetchAddress(37.56682, 126.97865);
+      setPosition({ lat: lat ?? 37.56682, lng: lng ?? 126.97865 });
+      // 기본 위치의 주소를 가져옴
+      fetchAddress(lat ?? 37.56682, lng ?? 126.97865);
     } else {
       // 현재 위치 정보가 있을 경우, 해당 위치로 설정
       setPosition({ lat: position.lat, lng: position.lng });
+      // 해당 위치의 주소를 가져옴
       fetchAddress(position.lat, position.lng);
     }
   }, []);
@@ -39,23 +63,18 @@ export function useKakaoMap(props: IUseKakaoMap) {
     geocoder.coord2Address(coord.getLng(), coord.getLat(), (result, status) => {
       if (status === window.kakao.maps.services.Status.OK) {
         const address = result[0]?.address?.address_name;
-        setAddress(address || "주소를 찾을 수 없습니다.");
-        console.log(address);
+        addOrUpdateQueryParams({ address });
       }
     });
   };
 
   // 지도 클릭 이벤트 - 클릭한 위치 좌표 저장
   const mapClick = (mouseEvent: any) => {
+    if (params.showmap === "false" || !params.showmap) return; // 지도가 열려있지 않거나 숨겨져있을 경우
     const lat = mouseEvent.latLng.getLat();
     const lng = mouseEvent.latLng.getLng();
-    setPosition({ lat, lng });
     fetchAddress(lat, lng);
-  };
-
-  // 지도 열기/닫기
-  const toggleMap = () => {
-    setOpenMap((prev) => !prev);
+    addOrUpdateQueryParams({ lat, lng });
   };
 
   return {
@@ -63,7 +82,8 @@ export function useKakaoMap(props: IUseKakaoMap) {
     mapClick,
     address,
     openMap,
-    toggleMap,
+    setOpenMap,
     setAddress,
+    setPosition,
   };
 }
