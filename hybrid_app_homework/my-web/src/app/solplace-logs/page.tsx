0b591f6a17189982e90d340fdfb9ed00e29593c3
jomira0220 <whalfk220@gmail.com>
2024-12-04 19:53:14 +0900

    SolplaceLogsList 컴포넌트로 플레이스 목록 표시 방식 변경

--- hybrid_app_homework/my-web/src/app/solplace-logs/page.tsx
+++ hybrid_app_homework/my-web/src/app/solplace-logs/page.tsx
@@ -3,48 +3,13 @@ import { FiMapPin } from "react-icons/fi";
 import { FaUserAlt } from "react-icons/fa";
 import { IoIosAdd } from "react-icons/io";
 import Footer from "@/commons/layout/footer";
-import Image from "next/image";
+
+import SolplaceLogsList from "@/components/solplace-logs/solplace-list";
 
 export default function SolPlaceListPage() {
   return (
     <>
-      {/* <div className="flex items-center justify-center text-sm min-h-[80vh] text-center text-[#f5f5f5">
-        등록된 플레이스가
-        <br /> 없습니다.
-      </div> */}
-
-      <div className="grid p-[1.25rem_1.5rem] gap-4 grid-cols-2 w-full">
-        {new Array(4).fill(0).map((_, i) => (
-          <div className="flex flex-col gap-2">
-            <div className="rounded-lg w-full overflow-hidden">
-              <Image
-                className="object-cover"
-                src="/images/sample_01.jpg"
-                width={375}
-                height={200}
-                alt=""
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <div>
-                <h3 className="text-base leading-[1.5rem] font-bold truncate">
-                  Bramble & Brioche 한남점
-                </h3>
-                <p className="truncate text-[#5f5f5f] text-sm">
-                  한국에서 느낄 수 없었던 그런 맛의 맛집
-                </p>
-              </div>
-
-              <div className="flex gap-2">
-                <div className="text-[0.8125rem] leading-5 font-semibold text-[#777] flex items-center gap-1 truncate">
-                  <FiMapPin size={14} color="#777" />
-                  서울특별시 용산구
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <SolplaceLogsList />
 
       <Footer className="border-t border-[#f2f2f2] p-[0.75rem_0_1.5rem]">
         <Link
