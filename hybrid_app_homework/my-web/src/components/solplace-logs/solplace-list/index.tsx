631f4fa742bf7c5232ab5d0ee6d30187358896f6
jomira0220 <whalfk220@gmail.com>
2024-12-04 20:01:28 +0900

    SolplaceLogsList 컴포넌트 추가: 무한 스크롤 기능을 통해 솔플레이스 로그 목록을 동적으로 로드

---
+++ hybrid_app_homework/my-web/src/components/solplace-logs/solplace-list/index.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useQuery } from "@apollo/client";
+import { FETCH_SOLPLACE_LOGS } from "@/commons/apis/graphql/queries/fetch-solplace-logs.query";
+import { useState } from "react";
+import SolPlaceItem from "@/components/solplace-logs/solplace-list/solplace-item";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+type TFetchSolplaceLogs = {
+  id: string;
+  images: string[];
+  userId: string;
+  title: string;
+  contents: string;
+  addressCity: string;
+  addressTown: string;
+};
+
+export default function SolplaceLogsList() {
+  const [hasMore, setHasMore] = useState(true);
+  const { data, fetchMore } = useQuery(FETCH_SOLPLACE_LOGS);
+  console.log("data", data);
+
+  const onNext = async () => {
+    if (!data) return;
+
+    await fetchMore({
+      variables: {
+        page: Math.ceil((data.fetchSolplaceLogs.length ?? 10) / 10) + 1,
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        const fetchMoreResultData = fetchMoreResult?.fetchSolplaceLogs;
+        if (!fetchMoreResultData?.length) {
+          setHasMore(false);
+          return prev;
+        }
+        return {
+          fetchSolplaceLogs: [
+            ...prev.fetchSolplaceLogs,
+            ...fetchMoreResult?.fetchSolplaceLogs,
+          ],
+        };
+      },
+    });
+  };
+
+  return (
+    <div>
+      {data ? (
+        <InfiniteScroll
+          dataLength={data?.fetchSolplaceLogs.length ?? 0}
+          next={onNext}
+          hasMore={hasMore}
+          className="grid p-[1.25rem_1.5rem] gap-4 grid-cols-2 w-full"
+          loader={<div>loading</div>}
+        >
+          {data?.fetchSolplaceLogs?.map((el: TFetchSolplaceLogs) => (
+            <SolPlaceItem key={el.id} {...el} />
+          ))}
+        </InfiniteScroll>
+      ) : (
+        <div className="flex items-center justify-center text-sm min-h-[80vh] text-center text-[#f5f5f5">
+          등록된 플레이스가
+          <br /> 없습니다.
+        </div>
+      )}
+    </div>
+  );
+}
