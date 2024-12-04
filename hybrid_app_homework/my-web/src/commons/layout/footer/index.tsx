0220c47a5f0b73935133f419636cade760dae1c7
jomira0220 <whalfk220@gmail.com>
2024-12-04 19:56:10 +0900

    Footer 컴포넌트 수정: clsx를 사용하여 클래스 이름 동적 처리 및 className prop 추가

--- hybrid_app_homework/my-web/src/commons/layout/footer/index.tsx
+++ hybrid_app_homework/my-web/src/commons/layout/footer/index.tsx
@@ -1,16 +1,27 @@
+"use client";
 import React from "react";
+import clsx from "clsx";
 
 export default function Footer({
   children,
   isFixed,
+  className,
 }: {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   isFixed?: boolean;
+  className?: string;
 }) {
   return (
     <>
       <div style={{ flex: 1 }}></div>
-      <footer className={`w-full p-4 ${isFixed ? "fixed" : ""}`}>
+      <footer
+        // className={`w-full p-4 ${isFixed ? "fixed" : ""}`}
+        className={clsx(
+          "w-full p-4",
+          isFixed && "fixed",
+          className && className
+        )}
+      >
         {children}
       </footer>
     </>
