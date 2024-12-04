fed234ccc03a7b277afc3c8cd93f795f9f0da1e9
jomira0220 <whalfk220@gmail.com>
2024-12-04 20:00:33 +0900

    SolplaceLogsMap 컴포넌트 수정: useSearchParams 제거 및 useParamsControl로 쿼리 파라미터 관리 개선

--- hybrid_app_homework/my-web/src/components/solplace-logs/edit-new-map/index.tsx
+++ hybrid_app_homework/my-web/src/components/solplace-logs/edit-new-map/index.tsx
@@ -1,42 +1,38 @@
+"use client";
+
 import KaKaoMap from "@/components/kakao-map";
-import { useSearchParams } from "next/navigation";
 import { useKakaoMap } from "@/commons/hooks/use-kakao-map";
+import { useParamsControl } from "@/commons/hooks/use-params-control";
 
 export default function SolplaceLogsMap() {
-  const searchParams = useSearchParams();
-  const params = Object.fromEntries(searchParams.entries());
-  const { mapClick, address, position } = useKakaoMap({
-    address: params.address || "서울특별시 중구 세종대로 110",
-    position: {
-      lat: Number(params.lat) || 37.5666,
-      lng: Number(params.lng) || 126.979,
-    },
-  });
+  const { addOrUpdateQueryParams } = useParamsControl();
+  const { address, position, openMap } = useKakaoMap();
 
-  return (
-    <div className="fixed left-0 top-0 z-auto w-screen h-screen flex flex-col justify-between">
-      <KaKaoMap
-        className="w-screen flex flex-1"
-        lat={position?.lat ?? null}
-        lng={position?.lng ?? null}
-      />
-      <div className="bg-white flex flex-col gap-5 p-5">
-        <input
-          className="rounded-3xl text-center h-12 shadow-[0px_0px_8px_0px_#00000029]"
-          value={params.address || address}
-          readOnly
-        />
+  if (openMap)
+    return (
+      <div className="fixed left-0 top-0 z-10 w-screen h-screen flex flex-col justify-between">
+        <KaKaoMap className="w-screen flex flex-1" />
+        <div className="bg-white flex flex-col gap-5 p-5">
+          <input
+            className="rounded-3xl text-center h-12 shadow-[0px_0px_8px_0px_#00000029]"
+            value={address}
+            readOnly
+          />
 
-        <button
-          className="bg-[var(--primary)] h-12 text-white font-bold text-lg leading-6 rounded-lg"
-          onClick={() => {
-            window.history.pushState(null, "", `?showmap=false`);
-            window.history.replaceState(null, "", `?showmap=false`);
-          }}
-        >
-          이 위치로 등록
-        </button>
+          <button
+            className="bg-[var(--primary)] h-12 text-white font-bold text-lg leading-6 rounded-lg"
+            onClick={() => {
+              addOrUpdateQueryParams({
+                showmap: "false",
+                lat: String(position.lat),
+                lng: String(position.lng),
+                address: address,
+              });
+            }}
+          >
+            이 위치로 등록
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
 }
