f4dadfa5db2c7459198074188e61971e5fdac6da
jomira0220 <whalfk220@gmail.com>
2024-12-10 17:47:39 +0900

    수업 내용 파일 업로드

--- hybrid-app/class-web/src/app/section09/09-01-view-transition-moved/page.tsx
+++ hybrid-app/class-web/src/app/section09/09-01-view-transition-moved/page.tsx
@@ -1,40 +1,40 @@
-"use client"
+"use client";
 
-import { useRoutingSettingViewTransition } from "@/commons/settings/09-01-routing-setting-view-transition/hook"
+import { useRoutingSettingViewTransition } from "@/commons/settings/09-01-routing-setting-view-transition/hook";
 
 export default function ViewTransitionMovedPage() {
-    const { onRoutingBack } = useRoutingSettingViewTransition()
+  const { onRoutingBack } = useRoutingSettingViewTransition();
 
-    return (
-        <div>
-            {/* 모바일 헤더 */}
-            <button onClick={onRoutingBack}>{`<< 뒤로가기`}</button>
+  return (
+    <div>
+      {/* 모바일 헤더 */}
+      <button onClick={onRoutingBack}>{`<< 뒤로가기`}</button>
 
-            {/* 모바일 컨텐츠 */}
-            <div 
-                style={{
-                    width: "100px",
-                    height: "100px",
-                    backgroundColor: "blue",
-                    margin: "10px"
-                }} 
-            />    
-            <div 
-                style={{
-                    width: "100px",
-                    height: "100px",
-                    backgroundColor: "blue",
-                    margin: "10px"
-                }} 
-            /> 
-            <div 
-                style={{
-                    width: "100px",
-                    height: "100px",
-                    backgroundColor: "blue",
-                    margin: "10px"
-                }} 
-            />  
-        </div>
-    )
+      {/* 모바일 컨텐츠 */}
+      <div
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: "blue",
+          margin: "10px",
+        }}
+      />
+      <div
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: "blue",
+          margin: "10px",
+        }}
+      />
+      <div
+        style={{
+          width: "100px",
+          height: "100px",
+          backgroundColor: "blue",
+          margin: "10px",
+        }}
+      />
+    </div>
+  );
 }
--- hybrid-app/class-web/src/commons/settings/09-01-routing-setting-view-transition/index.tsx
+++ hybrid-app/class-web/src/commons/settings/09-01-routing-setting-view-transition/index.tsx
@@ -1,20 +1,42 @@
-"use client"
+"use client";
 
-// import { useEffect } from "react"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-export default function RoutingSettingViewTransition({children}) {
+export default function RoutingSettingViewTransition({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // const router = useRouter();
+  // useEffect(() => {
+  //   // <Link /> (<a />: a태그)를 클릭했다면? 뷰-트랜지션 애니메이션 적용 처리
+  //   const viewTransition = (event) => {
+  //     // event.preventDefault();
+  //     const isAnchor = (event.target as Element).closest("a");
+  //     if (isAnchor) {
+  //       document.startViewTransition(() => {
+  //         router.push(isAnchor.href);
+  //       });
+  //     }
+  //   };
 
-    // useEffect(() => {
+  //   window.addEventListener("click", viewTransition);
 
-    //     // <Link /> (<a />)를 클릭했다면? 뷰-트랜지션 애니메이션
-    //     window.addEventListener("click", (event) => {
-    //         const isAnchor = event.target.closest("a")
-    //         if(isAnchor) {
-    //             document.startViewTransition()
-    //         }
-    //     })
+  //   return () => {
+  //     window.removeEventListener("click", viewTransition);
+  //   };
+  // }, []);
 
-    // }, [])
-
-    return <>{children}</>
+  return <>{children}</>;
 }
+
+/*
+01. 방법1 : 비효율적인 방법
+document.startViewTransition(() => {
+     router.push(event.target.href);
+})
+
+02. 방법2: 나만의 커스텀 링크 만들기
+a태그를 렌더링하는 컴포넌트를 새로 만들어서 사용하는 방법
+*/
