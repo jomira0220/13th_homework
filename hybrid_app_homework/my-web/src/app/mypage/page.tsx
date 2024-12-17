c4bb1fb76ba3824190a4587e1b7bb6f450530bd3
jomira0220 <whalfk220@gmail.com>
2024-12-17 10:42:38 +0900

    마이페이지 신규 생성

---
+++ hybrid_app_homework/my-web/src/app/mypage/page.tsx
@@ -0,0 +1,11 @@
+import FooterNavigation from "@/components/commons/footer-navigation";
+import MyPageComponent from "@/components/domain/mypage";
+
+export default function MyPage() {
+  return (
+    <>
+      <MyPageComponent />
+      <FooterNavigation />
+    </>
+  );
+}
