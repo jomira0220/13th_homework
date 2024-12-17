125359ccc61d7991cf66276ff58149056abb6c59
jomira0220 <whalfk220@gmail.com>
2024-12-17 10:41:25 +0900

    알림 기능 개선: 알림 스케줄 생성 함수의 매개변수 타입 정의 및 알림 응답 리디렉션 기능 추가

--- hybrid_app_homework/mobile/apis/use-device-notifications.ts
+++ hybrid_app_homework/mobile/apis/use-device-notifications.ts
@@ -10,20 +10,15 @@ Notifications.setNotificationHandler({
   }),
 });
 
-export const useDeviceNotifications = (onResponse: any) => {
+export const useDeviceNotifications = (onResponse: ) => {
   // 2. 알람 스케줄 생성
-  const createDeviceNotificationsForSolplaceLogNewSet = async (
-    variables: any
-  ) => {
-    const { solplaceLogId } = variables;
+  const createDeviceNotificationsForSolplaceLogNewSet = async (variables: {
+    title: string;
+    body: string;
+    data: { page: string };
+  }) => {
     await Notifications.scheduleNotificationAsync({
-      content: {
-        title: "솔플레이스 등록 완료",
-        body: "신규 솔플레이스로그가 등록되었습니다.",
-        data: {
-          page: `/solplace-logs/${solplaceLogId}`,
-        },
-      },
+      content: variables,
       trigger: null,
     });
     onResponse({
@@ -36,7 +31,6 @@ export const useDeviceNotifications = (onResponse: any) => {
   // 3. 알람 권한 요청
   const requestDeviceNotificationsForPermissionSolplaceLogNewSet = async () => {
     await Notifications.requestPermissionsAsync();
-
     onResponse({
       requestDeviceNotificationsForPermissionSolplaceLogNewSet: {
         message: "요청완료",
@@ -47,16 +41,17 @@ export const useDeviceNotifications = (onResponse: any) => {
   // 알림 권한 허용 헀는지 여부를 확인하는 함수
   const fetchDeviceNotificationsForPermissionSet = async () => {
     const result = await Notifications.getPermissionsAsync();
-
     onResponse({
       fetchDeviceNotificationsForPermissionSet: {
         status: result.status,
       },
     });
   };
+
   useEffect(() => {
+    // 알림 응답을 수신하고, 해당 응답 데이터를 기반으로 특정 페이지로 리디렉션하는 기능
     Notifications.addNotificationResponseReceivedListener((response) => {
-      const notificationData = response.notification.request.content.data;
+      const notificationData = response.notification.request.content.data; // 알람 데이터
       onResponse({ redirect: notificationData.page });
     });
   }, []);
