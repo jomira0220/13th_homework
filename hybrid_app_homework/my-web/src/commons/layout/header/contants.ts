4be04fa379ce6470d3f8c0d357a245018ebe78d0
jomira0220 <whalfk220@gmail.com>
2024-12-18 10:13:18 +0900

    헤더 타입에 회원가입 및 내 설정 경로 추가, 수정된 경로에 backUrl 속성 추가

--- hybrid_app_homework/my-web/src/commons/layout/header/contants.ts
+++ hybrid_app_homework/my-web/src/commons/layout/header/contants.ts
@@ -19,12 +19,20 @@ export const HeaderType = (params: Params, queryParams?: { showmap?: string }): 
 
   return {
   GLOBAL: {
+    "/mypage": { hasBack:false, title:"내 설정", isTransparent:false, isNotchTransparent:false },
     "/solplace-logs/new": { hasBack: true, title: "플레이스 등록", isTransparent: false, isNotchTransparent: false },
     "/solplace-logs": { hasBack: false, title: "플레이스", isTransparent: false, isNotchTransparent: false },
+    "/signup": { hasBack: true, title: "회원가입", isTransparent: false, isNotchTransparent: false, backUrl: "/login" },
   },
   LOCAL:{
+    "/login": { hasBack: false, title: "", isTransparent: false, isNotchTransparent: false },
     [`/solplace-logs/${params.Id}`]: { hasBack: true, title: "", isTransparent: true, isNotchTransparent: true },
-    [`/solplace-logs/${params.Id}/edit`]: { hasBack: true, title: showmap ? "" : "플레이스 수정", isTransparent: showmap ? true : false, isNotchTransparent: showmap ? true : false },
+    [`/solplace-logs/${params.Id}/edit`]: { 
+      hasBack: true, title: showmap ? "" : "플레이스 수정", 
+      isTransparent: showmap ? true : false, 
+      isNotchTransparent: showmap ? true : false,
+      backUrl: `/solplace-logs/${params.solplaceLogId}`,
+     },
   }
 }
 };
