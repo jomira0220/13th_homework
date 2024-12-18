6f408105c245457780f7b455fb68b6acfe695aec
jomira0220 <whalfk220@gmail.com>
2024-12-18 10:30:03 +0900

    Input 컴포넌트의 타입 개선 및 비밀번호 표시 기능 추가

--- hybrid_app_homework/my-web/src/components/commons/input/index.tsx
+++ hybrid_app_homework/my-web/src/components/commons/input/index.tsx
@@ -1,21 +1,28 @@
 "use client";
 
 import { useFormContext } from "react-hook-form";
+import type { FieldValues, Path } from "react-hook-form";
+import {
+  useState,
+  type HTMLInputTypeAttribute,
+  type InputHTMLAttributes,
+} from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
-interface IInputProps {
+interface IInputProps<T> extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
   placeholder?: string;
   readOnly?: boolean;
   title?: string;
-  type: string;
-  name: string;
+  type: HTMLInputTypeAttribute;
+  name: Path<T>;
   isError?: boolean;
   hidden?: boolean;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   required?: boolean;
 }
 
-export default function Input({
+function BaseInput<T extends FieldValues>({
   placeholder,
   readOnly,
   title,
@@ -24,8 +31,10 @@ export default function Input({
   isError = false,
   hidden = false,
   required,
-}: IInputProps) {
-  const { register, formState } = useFormContext();
+  ...rest
+}: IInputProps<T>) {
+  const { register, formState } = useFormContext<T>();
+  const [isPassword, setIsPassword] = useState<boolean>(false);
 
   return (
     <label className="flex flex-col gap-2">
@@ -37,22 +46,38 @@ export default function Input({
           {required && <span className="text-red-600 leading-none">*</span>}
         </div>
       )}
-      <input
-        className="h-11 font-medium rounded-lg border border-gray-200 px-4 placeholder:text-gray-400"
-        type={type}
-        placeholder={placeholder}
-        readOnly={readOnly}
-        hidden={hidden}
-        {...register(name, {
-          required: required,
-        })}
-      />
+      <div className="relative">
+        <input
+          className="h-11 font-medium rounded-lg border border-gray-200 px-4 placeholder:text-gray-400 w-full"
+          type={type === "password" ? (isPassword ? "text" : "password") : type}
+          placeholder={placeholder}
+          readOnly={readOnly}
+          hidden={hidden}
+          {...register(name, {
+            required: required,
+          })}
+          {...rest}
+        />
+        {type === "password" && (
+          <div
+            className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer"
+            onClick={() => setIsPassword(!isPassword)}
+          >
+            {isPassword ? <FaEye color="#777" /> : <FaEyeSlash color="#777" />}
+          </div>
+        )}
+      </div>
 
-      {isError && formState.errors && (
-        <div className="text-red-600">
+      {isError && formState.errors[name]?.message?.toString() && (
+        <div className="text-red-600 font-medium text-xs">
           {formState.errors[name]?.message?.toString()}
         </div>
       )}
     </label>
   );
+}
+
+export function Input<T extends FieldValues>(props: IInputProps<T>) {
+  console.log("Input", props);
+  return <BaseInput<T> {...props} />;
 }
