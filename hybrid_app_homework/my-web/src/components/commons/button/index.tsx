41e5f575fcfba13fc9f16c67863cc0d9a87c8c0d
jomira0220 <whalfk220@gmail.com>
2024-12-18 10:28:21 +0900

    SubmitButton 컴포넌트의 폼 유효성 검사 로직 수정: 경로에 따라 버튼 활성화 조건 변경

--- hybrid_app_homework/my-web/src/components/commons/button/index.tsx
+++ hybrid_app_homework/my-web/src/components/commons/button/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { usePathname } from "next/navigation";
 import { ButtonHTMLAttributes } from "react";
 import { useFormContext } from "react-hook-form";
 import type { FieldValues } from "react-hook-form";
@@ -8,19 +9,22 @@ interface IButtonBase extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export default function ButtonBase<T extends FieldValues>({
+export function SubmitButton<T extends FieldValues>({
   children,
+  onClick,
   ...rest
 }: IButtonBase) {
+  const pathname = usePathname();
   const { formState } = useFormContext<T>();
-  const isFormValid = formState ? formState.isValid || formState.isDirty : true; // formState의 isValid 값이 true이면 버튼 활성화
-  console.log(
-    "formContext.formState.isValid",
-    formState.isValid,
-    formState.isDirty
-  );
+  const formStateCheck = pathname.includes("edit")
+    ? formState.isValid || formState.isDirty
+    : formState.isValid && formState.isDirty;
+  // isValid : 폼의 유효성 검사를 통과했는지 여부
+  // isDirty : 폼의 값이 변경되었는지 여부
+  const isFormDisabled = formState ? formStateCheck : true;
+
   return (
-    <button disabled={!isFormValid} {...rest}>
+    <button disabled={!isFormDisabled} {...rest}>
       {children}
     </button>
   );
