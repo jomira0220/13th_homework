ec84111c07388147b1ec5248d61161c0f23ea386
jomira0220 <whalfk220@gmail.com>
2024-12-18 10:16:24 +0900

    useDeviceSetting 훅에 뒤로가기 이벤트 처리 기능 추가 및 웹뷰 로그 기록

--- hybrid_app_homework/my-web/src/commons/settings/device-setting/hook.ts
+++ hybrid_app_homework/my-web/src/commons/settings/device-setting/hook.ts
@@ -1,4 +1,8 @@
+"use client";
+
+import { webviewLog } from "@/commons/libraries/webview-log";
 import { FETCH_DEVICE_KEY } from ".";
+import { useRouter, usePathname } from "next/navigation";
 
 declare const window: Window & {
   ReactNativeWebView: {
@@ -6,16 +10,54 @@ declare const window: Window & {
   };
 };
 
+// 종료되어야할 페이지
+const closePage = ["/solplace-logs", "/mypage"];
+
 export const useDeviceSetting = () => {
-  const fetchApp = async <T>({ query, variables = {} }: { query: string; variables?: any }): Promise<T> => {
+  const fetchApp = async <T>({
+    query,
+    variables = {},
+  }: {
+    query: string;
+    variables?: any;
+  }): Promise<T> => {
     const result = await new Promise<T>((resolve) => {
       FETCH_DEVICE_KEY[query] = resolve;
-      window.ReactNativeWebView.postMessage(JSON.stringify({ query, variables }));
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({ query, variables })
+      );
     });
     return result;
   };
 
+  const router = useRouter();
+  const pathname = usePathname();
+
+  // 뒤로가기 이벤트 => 종료되어야할 페이지인지 확인 후 종료되어야할 페이지라면 앱 종료 이벤트 호출
+  const onRoutingBack = (backUrl?: string) => {
+    const isExitPage = closePage.includes(pathname);
+    if (isExitPage) return fetchApp({ query: "exitDeviceRoutingForBackSet" });
+
+    // 뒤로가기 이벤트 발생시 html에 backMove 클래스 추가
+    document.documentElement.classList.add("backMove");
+
+    if (document.startViewTransition) {
+      return document
+        .startViewTransition(() => {
+          webviewLog("startViewTransition 실행");
+          // 뒤로가기 이벤트 발생시 backUrl이 있으면 해당 페이지로 이동, 없으면 뒤로가기
+          backUrl ? router.push(backUrl) : router.back();
+        })
+        .finished.finally(() => {
+          document.documentElement.classList.remove("backMove");
+        });
+    } else {
+      router.back();
+    }
+  };
+
   return {
     fetchApp,
+    onRoutingBack,
   };
 };
