5e5fcd8b2c85a4d4d4fb94bf66bc2d29d6c7229a
jomira0220 <whalfk220@gmail.com>
2024-12-18 10:27:05 +0900

    이미지 업로드 스토어에 이미지 초기화 기능 추가

--- hybrid_app_homework/my-web/src/commons/stores/image-upload.ts
+++ hybrid_app_homework/my-web/src/commons/stores/image-upload.ts
@@ -3,9 +3,11 @@ import { create } from "zustand";
 type ImageState = {
   uploadImages: string[];
   setUploadImages: (images: string[]) => void;
+  clearImages: () => void;
 };
 
 export const useImageUploadStore = create<ImageState>((set) => ({
   uploadImages: [],
   setUploadImages: (uploadImages: string[]) => set({ uploadImages }),
+  clearImages: () => set({ uploadImages: [] }),
 }));
